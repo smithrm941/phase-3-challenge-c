01d201cd1001643e5f0ea9c94eb58b51a4c44ec6
smithrm941 <smithrm941@gmail.com>
2017-08-14 15:14:08 -0700

    working post route in part 1

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Part 1: Simple web app
 
+
+
 ## Part 2: Command Line Hotel Management System
 
 ## Part 3: Web interface for hotel manager
--- part-1/app.js
+++ part-1/app.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express();
+
+app.use(bodyParser.json())
+
 
 app.get('/api/shout/:word', (request, response) => {
   const { word } = request.params
   response.status(200).send(word.toUpperCase() + '!!!')
+})
+
+app.post('/api/array/merge', (request, response) => {
+  const a = request.body.a
+  const b = request.body.b
+    if(Array.isArray(a) && Array.isArray(b)){
+      const result = a.concat(b)
+      console.log(result)
+      response.json({'result': result})
+    } else if (!Array.isArray(a) || !Array.isArray(b)) {
+      response.status(400).json({"error": 'Input data should be type of array.'})
+    }
 })
 
 app.listen(3000, () => {
