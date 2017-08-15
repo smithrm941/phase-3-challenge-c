4cdfae343a1c45f4452100d30cf370c532a90c48
smithrm941 <smithrm941@gmail.com>
2017-08-14 18:48:10 -0700

    complete schema for hotel db

---
+++ part-2/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS hotel_db;
+CREATE DATABASE hotel_db;
+
+\c hotel_db
+
+CREATE TABLE guest (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255)
+);
+
+CREATE TABLE room (
+  id SERIAL PRIMARY KEY,
+  room_number VARCHAR(2),
+  capacity INT
+);
+
+CREATE TABLE booking (
+  id SERIAL PRIMARY KEY,
+  room_id INT REFERENCES room(id),
+  guest_id INT REFERENCES guest(id),
+  check_in DATE,
+  check_out DATE
+);
