949af8de9dcc124779259471025b7d67cd24274d
smithrm941 <smithrm941@gmail.com>
2017-08-14 21:53:37 -0700

    queries for all upcoming bookings, and upcoming bookings by room

--- part-2/database.js
+++ part-2/database.js
@@ -3,16 +3,38 @@ const connectionString = 'postgres://localhost:5432/grocery_store'
 const db = pgp(connectionString)
 
 const listAllGuests = () => { return db.any(
-  `SELECT * FROM guest`
+  `SELECT *
+  FROM guest`
 )}
 
 const listAllRooms = () => {return db.any (
-  `SELECT room_number, capacity, available FROM room ORDER BY room_number`
+  `SELECT room_number, capacity, available
+  FROM room
+  ORDER BY room_number`
 )}
 
 const listAvailableRooms = () => {return db.any (
-  `SELECT room_number, capacity, available FROM room WHERE available = true ORDER BY room_number`
+  `SELECT room_number, capacity, available
+  FROM room
+  WHERE available = true ORDER BY room_number`
+)}
+
+const listUpcomingBookings = () => {return db.any(
+  `SELECT room.room_number, guest.name, booking.check_in, booking.check_out
+  FROM room, guest, booking
+  WHERE room.id = booking.room_id
+  AND guest.id = booking.guest_id
+  AND booking.check_out > CURRENT_DATE;`
+)}
+
+const listBookingsForRoom = (room) => {return db.any (
+  `SELECT room.room_number, guest.name, booking.check_in, booking.check_out
+  FROM room, guest, booking
+  WHERE room.room_number = $1
+  AND room.id = booking.room_id
+  AND guest.id = booking.guest_id
+  AND booking.check_out > CURRENT_DATE;`, [room]
 )}
 
 
-module.exports = { listAllGuests, listAllRooms, listAvailableRooms }
+module.exports = { listAllGuests, listAllRooms, listAvailableRooms, listUpcomingBookings, listBookingsForRoom }
