a52babda554902966e23684e1b888a4fa795f95b
smithrm941 <smithrm941@gmail.com>
2017-08-15 01:29:19 -0700

    WIP command line functionality

---
+++ part-2/hotel.js
@@ -0,0 +1,25 @@
+const query = require('./database')
+
+switch(process.argv[2]){
+  case 'guests':
+    //call function that lists guests
+    break;
+  case 'rooms':
+    if(process.argv[3] === '--available'){
+      //call function that lists available rooms
+    } else {
+      //call function that lists all rooms
+    }
+    break;
+  case 'bookings':
+    if(process.argv[3].length === 2){ //all room numbers are 2 characters
+      //call function that lists upcoming bookings for specific rooms
+    } else {
+      //call function that lists all upcoming bookings
+    }
+    break;
+  default:
+    console.log(`Please use the commands 'guests', 'rooms',
+    'rooms --available', or 'bookings'`)
+    break;
+}
