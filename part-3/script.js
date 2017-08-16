76006b03cf9adf8bc016a39a3ce3c1bae2c6ab4e
smithrm941 <smithrm941@gmail.com>
2017-08-16 02:38:50 -0700

    modal displays room number

--- part-3/script.js
+++ part-3/script.js
@@ -4,13 +4,24 @@ const bookingInfo = document.getElementById('bookingModal')
 const closeBookingInfo = document.querySelector('.close')
 
 // temp table stuff
-const roomsTable = document.getElementById('room-table')
+const bookedRoomNumber = document.getElementById('room-number')
 
 bookButtons.forEach(function(bookButton){
   bookButton.addEventListener("click", () =>{
+    let associatedRow = bookButton.parentNode.parentNode
+    let associatedDataSet = associatedRow.children;
+    let dataForBooking = [];
+    // console.log(associatedData)
+    for (let i = 0; i < associatedDataSet.length - 1; i++) {
+      let associatedDataCell = associatedDataSet[i].innerHTML;
+      dataForBooking.push(associatedDataCell)
+    }
+    // let associatedRoomNo = dataForBooking[0]
+    // let associatedCapacity = dataForBooking[1]
+    // let associatedPrice = dataForBooking[2]
+    // console.log(dataForBooking)
+    bookedRoomNumber.innerHTML = dataForBooking[0]
     bookingInfo.style.display = "block";
-    let tableRow = bookButton.parentNode.parentNode
-    console.log(tableRow)
   })
 })
 
