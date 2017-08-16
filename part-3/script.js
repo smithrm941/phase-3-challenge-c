fa0cd5da163129cc9d7b2cb4d788a67b6f2d7c68
smithrm941 <smithrm941@gmail.com>
2017-08-15 18:44:33 -0700

    book buttons display modal, modal's close button works

--- part-3/script.js
+++ part-3/script.js
@@ -1,1 +1,13 @@
-console.log('working')
+const bookButtons = document.querySelectorAll('.book-button')
+const bookingInfo = document.getElementById('bookingModal')
+const closeBookingInfo = document.querySelector('.close')
+
+bookButtons.forEach(function(bookButton) {
+  bookButton.addEventListener("click", () =>{
+    bookingInfo.style.display = "block";
+  })
+})
+
+closeBookingInfo.addEventListener("click", () => {
+  bookingInfo.style.display = "none";
+})
