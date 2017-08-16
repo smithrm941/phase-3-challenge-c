eff9af690b6929f6ff8c2e5e0859b5386bcdddac
smithrm941 <smithrm941@gmail.com>
2017-08-16 02:19:08 -0700

    can access parent row for button clicked

--- part-3/script.js
+++ part-3/script.js
@@ -1,10 +1,16 @@
+//display the modal and close it:
 const bookButtons = document.querySelectorAll('.book-button')
 const bookingInfo = document.getElementById('bookingModal')
 const closeBookingInfo = document.querySelector('.close')
 
-bookButtons.forEach(function(bookButton) {
+// temp table stuff
+const roomsTable = document.getElementById('room-table')
+
+bookButtons.forEach(function(bookButton){
   bookButton.addEventListener("click", () =>{
     bookingInfo.style.display = "block";
+    let tableRow = bookButton.parentNode.parentNode
+    console.log(tableRow)
   })
 })
 
